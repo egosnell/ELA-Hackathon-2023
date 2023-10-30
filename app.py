cf0eb6adc811cbbe32ae60abd29900a6ed50bb95
BrianNewton <brian.newton@uwaterloo.ca>
2023-10-30 18:30:48 -0400

    Added .JS for tabs

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def hello_world():
     return render_template("index.html", title="Hello")
+
+if __name__ == '__main__':
+   app.run(debug = True)
