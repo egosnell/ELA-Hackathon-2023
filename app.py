165323ea97e22fd3df920c1bc73601f66897f6b5
egosnell <89826840+egosnell@users.noreply.github.com>
2023-11-03 00:02:19 -0400

    Change Pages etc
    Added some functionality and a different bootstrap navbar, also added ability so that active tab is highlighted. Added some html stuff for the about us page, but css doesn't seem to work...

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
-def hello_world():
-    return render_template("index.html", title="Hello")
+def Home():
+    return render_template("home.html", title="Catfish Coders")
+
+@app.route("/About_ACAP", methods = ['GET', 'POST'])
+def About_ACAP():
+    active_page = "about"
+    return render_template("about.html", title="Catfish Coders", active_page=active_page)
+
+@app.route("/The_Problem", methods = ['GET', 'POST'])
+def The_Problem():
+    active_page = "problem"
+    return render_template("problem.html", title="Catfish Coders", active_page=active_page)
+
+@app.route("/Data", methods = ['GET', 'POST'])
+def Data():
+    active_page = "data"
+    return render_template("data.html", title="Catfish Coders", active_page=active_page)
 
 if __name__ == '__main__':
    app.run(debug = True)
